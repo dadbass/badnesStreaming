b6ec0977f894f10d9ab7e47b9048381ab5f84663
dadbass <45670534+dadbass@users.noreply.github.com>
2018-12-14 12:20:17 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`iTzMonTross`,"http://twitch.tv/JustSad")
+client.user.setGame(`God Knows Best. 😉 `,"http://twitch.tv/éMyyy")
 client.user.setStatus("dnd")
 });
 
